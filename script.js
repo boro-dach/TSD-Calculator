cab181009e220c138f6939667577605d99297e49
boro-dach <monstriqq1@gmail.com>
2023-07-25 20:59:43 +0200

    TSD-Calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+// document.querySelector('.form__object').addEventListener('click', objChoose)
+
+// function objChoose(){
+//     console.log('clicked!');
+//     var clElement = document.querySelector('.form__object');
+//     if(clElement.classList.contains('unactive') == true){
+//         clElement.classList.remove('unactive');
+//         console.log('removed unact')
+//         clElement.classList.add('active');
+//         console.log('added act')
+//         var pastElement = clElement
+//     }
+//     else{
+//         console.log('but is alrd act')
+//         pastElement.classList.remove('active')
+//         pastElement.classList.add('unactive')
+//         clElement.classList.add('active')
+//     }
+    
+// }
