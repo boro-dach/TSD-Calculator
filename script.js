fe9bd6ae45f651fc54e145a73d543ba719094621
boro-dach <monstriqq1@gmail.com>
2023-07-27 19:51:03 +0200

    TSD-Calculator
    finished!

--- script.js
+++ script.js
@@ -1,15 +1,71 @@
- document.querySelector('.form__object').addEventListener('click', checkAct)
+let timeButt = document.querySelector('.time');
+timeButt.addEventListener('click', timeClicked);
+let speedButt = document.querySelector('.speed');
+speedButt.addEventListener('click', speedClicked);
+let distanceButt = document.querySelector('.distance');
+distanceButt.addEventListener('click', distanceClicked);
+let active = 'time';
 
-function checkAct(){
-    let activeTab = document.activeElement.classList
-    if(activeTab.classList.contains('active')){
-        document.querySelector('.form__object').classList.remove('unactive')
-        document.querySelector('.form__activness').remove()
+document.querySelector('.submit').addEventListener('click', calculate);
+
+function timeClicked(){
+    document.getElementsByClassName('first_input')[0].placeholder = 'Distance: (km)';
+    document.getElementsByClassName('second_input')[0].placeholder = 'Speed: (km/h)';
+    active = 'time';
+    console.log(active);
+}
+
+function speedClicked(){
+    document.getElementsByClassName('first_input')[0].placeholder = 'Distance: (km)';
+    document.getElementsByClassName('second_input')[0].placeholder = 'Time: (h)';
+    active = 'speed';
+    console.log(active);
+}
+
+function distanceClicked(){
+    document.getElementsByClassName('first_input')[0].placeholder = 'Speed: (km/h)';
+    document.getElementsByClassName('second_input')[0].placeholder = 'Time: (h)';
+    active = 'dis';
+    console.log(active);
+}
+
+function calculate(){
+    console.log(`counting ${active}`)
+    if (active == 'time'){
+        let distance = document.getElementsByClassName('first_input')[0].value;
+        let speed = document.getElementsByClassName('second_input')[0].value;
+        let result = distance / speed;
+        if(isNaN(result) == true){
+            document.getElementsByTagName('p')[0].innerText = 'You have to input data first!';
+        }
+        else{
+            document.getElementsByTagName('p')[0].innerText = `It will take ${result} hour(s) to pass ${distance} kilometer(s) wiith speed of ${speed} kph.`;
+        }
+        
+    }
+    else if(active == 'speed'){
+        let distance = document.getElementsByClassName('first_input')[0].value;
+        let time = document.getElementsByClassName('second_input')[0].value;
+        let result = distance / time;
+        if(isNaN(result) == true){
+            document.getElementsByTagName('p')[0].innerText = 'You have to input data first!';
+        }
+        else{
+            document.getElementsByTagName('p')[0].innerText = `You have to move with a speed of ${result} kph to pass ${distance} kilometer(s) in ${time} hour(s).`;
+        }
+    }
+    else if(active == 'dis'){
+        let speed = document.getElementsByClassName('first_input')[0].value;
+        let time = document.getElementsByClassName('second_input')[0].value;
+        let result = speed * time;
+        if(isNaN(result) == true){
+            document.getElementsByTagName('p')[0].innerText = 'You have to input data first!';
+        }
+        else{
+            document.getElementsByTagName('p')[0].innerText = `You will pass ${result} kilometer(s) if you move with a speed of ${speed} kph for ${time} hour(s).`;
+        }
     }
     else{
-        document.querySelector('.form__object').classList.add('unactive')
-        const para = document.createElement('p')
-        const node = document.createTextNode('select tab first')
-        para.appendChild(node)
+        document.getElementsByTagName('p')[0].innerText = 'Oops.. Something went wrong :(';
     }
-} 
+}
